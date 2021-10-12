fa406fd2b13b509ca8ae70132c95b295d62f2a82
Chirag Jain <chiragj1812@gmail.com>
2021-10-12 16:41:33 +0530

    added script to automate first time download of embeddings for heroku

--- main/views.py
+++ main/views.py
@@ -1,7 +1,48 @@
 from django.shortcuts import render
 from emojifier_DeepLearningCode import predict
 # Create your views here.
+from decouple import config
+DOWNLOAD = config('DOWNLOAD',cast=bool, default=False)
+
+import requests
+
+def download_file_from_google_drive(id, destination):
+    URL = "https://docs.google.com/uc?export=download"
+
+    session = requests.Session()
+
+    response = session.get(URL, params = { 'id' : id }, stream = True)
+    token = get_confirm_token(response)
+
+    if token:
+        params = { 'id' : id, 'confirm' : token }
+        response = session.get(URL, params = params, stream = True)
+
+    save_response_content(response, destination)    
+
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith('download_warning'):
+            return value
+
+    return None
+
+def save_response_content(response, destination):
+    CHUNK_SIZE = 32768
+
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(CHUNK_SIZE):
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+def execute_download():
+    # emojifier_DeepLearningCode\embedding\glove.6B.50d.txt
+    download_file_from_google_drive('1MTlxFUh2PnT68HKvnUdTa4jpycRFb13O','emojifier_DeepLearningCode/embedding/glove.6B.50d.txt')
+
+
 def predictEmoji(request):
+    if DOWNLOAD:
+        print("ONE TIME DOWNLOAD OF EMBEDDINGS FILE ")
+        execute_download()
     if request.method=="GET":
         return render(request,'main/index.html')
     if request.method=="POST":
