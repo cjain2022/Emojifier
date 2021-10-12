477d7d9e67b90327b2c599b9f18fb6ed8f28d9df
Chirag Jain <chiragj1812@gmail.com>
2021-10-12 15:11:56 +0530

    decoupled environment variables

--- emojifierProject/settings.py
+++ emojifierProject/settings.py
@@ -12,20 +12,20 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os 
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n^ap7#czvfkcgfi+@^t7--75xa+llrv)cef91*4oyuyn815yg$'
+SECRET_KEY = config('SECRET_KEY', default='')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG',cast=bool, default=True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    host.strip()
+    for host in config('ALLOWED_HOSTS',default='').split(',')
+]
 
 
 # Application definition
